bd2a5e82dc0952768944d113b1692579146062c9
Muhammad Rais Fawwazi <MUHAMMAD269@e.ntu.edu.sg>
2018-02-08 18:32:45 +0800

    initial commit

---
+++ DSPIntro.m
@@ -0,0 +1,47 @@
+% Example 1
+x = [1 4 6];
+h = [4 -3 2];
+y = conv(x, h);
+
+% Defining Vectors and Matrices
+rowVecA = [1 4 2];
+colVecB = [2; 1; 3;];
+matC = [1 3 5; 3 2 6; 1 1 3];
+
+% Generate a vector containing numbers 30 to 50
+num = [30:50];
+
+% Generate a vector containing numbers 30 to 50, count in steps of 2
+eveNum = [30:2:50];
+
+% Referencing elements
+rowVecA(2);
+matC(2, 3);
+rowVecA(2:3);
+rowVecA(end);
+lenVecA = length(rowVecA);
+
+% Transpose Matrix/Vector
+transpVecB = colVecB';
+transpmatC = matC';
+
+% Matrix/Vector Multiplication
+A = [1 3 4; 2 4 7]
+B = [5; 6; 1];
+C = A * B;
+
+% Dot Multiplication
+D = [2 8 1; 1 1 5];
+E = A .* D;
+
+% Simultaneous Equations
+A = [3 4 -2; 4 -6 2; 2 1 0.2];
+p = [6; 1; 2];
+% q = [x; y; z]
+% Aq = p
+q = inv(A) * p;
+
+% Simultaneous Equations - Singular Matrix Example
+A = [3 4; 6 8];
+p = [6; 12];
+q = inv(A) * p;
