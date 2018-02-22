06852096bdbfd3359151df47139b95fb5881fff8
Muhammad Rais Fawwazi <MUHAMMAD269@e.ntu.edu.sg>
2018-02-22 23:24:26 +0800

    week 6 tutorial

--- DSPIntro.m
+++ DSPIntro.m
@@ -26,7 +26,7 @@ transpVecB = colVecB';
 transpmatC = matC';
 
 % Matrix/Vector Multiplication
-A = [1 3 4; 2 4 7]
+A = [1 3 4; 2 4 7];
 B = [5; 6; 1];
 C = A * B;
 
@@ -44,4 +44,49 @@ q = inv(A) * p;
 % Simultaneous Equations - Singular Matrix Example
 A = [3 4; 6 8];
 p = [6; 12];
-q = inv(A) * p;
+cond(A);
+% q = inv(A) * p;
+
+% Complex Numbers
+val = 3 + 2j;
+cplAry = [2; 3; 9] + j * [0.2; 3; 0.5];
+cplAry = [2 + 0.2j; 3 + 3j; 9 + 0.5j];
+
+% Magnitude of Complex Numbers
+absAry = abs(cplAry);
+
+% Phase of Complex Numbers
+phAry = phase(cplAry);
+
+% Angles in Degrees
+phAry = phase(cplAry) .* 180/pi;
+
+% Extract Real Part
+rlAry = real(cplAry);
+
+% Extract Real Part of Particular Element
+real(cplAry(3));
+
+% Extract Imaginary Part
+imAry = imag(cplAry);
+
+% Plotting
+
+% Create a new figure
+figure(20);
+
+% Specify a vector containing abscissa (counting from 100 to 200)
+x = [100:200];
+
+% Compute the ordinate values
+y = 3 * x + 10;
+
+% Plot the function
+plot(x, y);
+
+% Label the axes
+xlabel('x values')
+ylabel('y values')
+
+% Plot multiple equations on the same graph
+hold on;
